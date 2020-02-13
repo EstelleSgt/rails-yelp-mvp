7a51a0ad790671c4032d22b3df932f9278ffbf82
Estelle Saget <estelle.saget@live.fr>
2020-02-13 11:04:46 +0100

    Rails yelp mvp : ok

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
-  RATING = [0, 1, 2, 3, 4, 5].freeze
   validates :content, :rating, presence: true
-  validates :rating, inclusion: { in: RATING }
-  validates :rating, numericality: true
+  validates :rating, inclusion: { in: 0..5 }
+  validates :rating, numericality: { only_integer: true}
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian" }
+suhsi_place = { name: "Sushi Place", address: "21 King Street, London E3 1MD", category: "japanese" }
+le_bourguignon = { name: "Le Bourguignon", address: "35 High Park Avenue, London E4 2NQ", category: "french" }
+frites_land = { name: "Frites Land", address: "2 Camden Street, London E2 6RQ", category: "belgian" }
+
+[dishoom, pizza_east, suhsi_place, le_bourguignon, frites_land].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
